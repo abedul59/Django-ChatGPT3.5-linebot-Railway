fab5fa6cec3b5f41cbdc1eb403ac037ab1377f01
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-03-20 11:42:23 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -61,15 +61,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 DATABASES = {
-	    'default': {
-	        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-	        'NAME': os.environ["PGDATABASE"],
-	        'USER': os.environ["PGUSER"],
-	        'PASSWORD': os.environ["PGPASSWORD"],
-	        'HOST': os.environ["PGHOST"],
-	        'PORT': os.environ["PGPORT"],
-	    }
-	}
+'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'mydatabase',
+    'USER': 'mydatabaseuser',
+    'PASSWORD': 'my168password',
+    'HOST': '127.0.0.1',
+    'PORT': '5432',
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
